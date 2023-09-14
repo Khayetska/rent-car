79f521f4bf4672baf2d7ab7725a309251bb0efeb
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-09-14 20:18:46 +0000

    Deploying to gh-pages from @ Khayetska/rent-car@36c3288b62e85f99ecfd19f4e520e4d42a623db2 🚀

---
+++ static/js/607.3d0738e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[607],{607:function(e,r,t){t.r(r),t.d(r,{default:function(){return i}});t(791);var n="Hero_home__J31r9",c="Hero_heroSection__IeNtu",o=t(184),s=c,u=n;function a(){return(0,o.jsx)("section",{className:s,children:(0,o.jsx)("div",{className:u,children:(0,o.jsx)("p",{children:"Hero"})})})}function i(){return(0,o.jsx)(a,{})}}}]);
+//# sourceMappingURL=607.3d0738e2.chunk.js.map
