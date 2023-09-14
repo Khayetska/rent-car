69a00a67932be3a484c712cc6d4c549039c8ddbe
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-09-14 23:29:20 +0300

    Add styles for car card

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -3,6 +3,7 @@ import sass from './CarItem.module.scss';
 import placeholder from 'images/placeholder.jpeg';
 
 const {
+  carItem,
   carImgWrapper,
   carImg,
   carImgOverlay,
@@ -34,33 +35,37 @@ export default function CarItem({ car }) {
 
   console.log('IntelliSafe advanced safety feature'.slice(0, 27));
   return (
-    <li>
-      <div className={carImgWrapper}>
-        <img
-          src={img ?? placeholder}
-          className={carImg}
-          alt={img ? 'View of the car for rent' : 'There is no available image'}
-        />
-        <div className={carImgOverlay}></div>
-      </div>
-      <div className={infoWrapper}>
-        <div className={basicInfo}>
-          <h3 className={carTitle}>
-            {make} {model.length > 12 || make.length > 12 ? <br /> : ''}
-            <span className={carTitleAccent}>{model}</span>, {year}
-          </h3>
-          <p className={carTitle}>{rentalPrice}</p>
+    <li className={carItem}>
+      <div>
+        <div className={carImgWrapper}>
+          <img
+            src={img ?? placeholder}
+            className={carImg}
+            alt={
+              img ? 'View of the car for rent' : 'There is no available image'
+            }
+          />
+          <div className={carImgOverlay}></div>
         </div>
-        <div className={extraInfo}>
-          <p className={extraInfoText}>
-            {city} | {country} | {rentalCompany}
-          </p>
-          <p className={extraInfoText}>
-            {type} | {model} | {mileage} |{' '}
-            {functionalities[0].length > 25
-              ? `${functionalities[0].slice(0, 25)}...`
-              : functionalities[0]}
-          </p>
+        <div className={infoWrapper}>
+          <div className={basicInfo}>
+            <h3 className={carTitle}>
+              {make} {model.length > 12 || make.length > 12 ? <br /> : ''}
+              <span className={carTitleAccent}>{model}</span>, {year}
+            </h3>
+            <p className={carTitle}>{rentalPrice}</p>
+          </div>
+          <div className={extraInfo}>
+            <p className={extraInfoText}>
+              {city} | {country} | {rentalCompany}
+            </p>
+            <p className={extraInfoText}>
+              {type} | {model} | {mileage} |{' '}
+              {functionalities[0].length > 25
+                ? `${functionalities[0].slice(0, 25)}...`
+                : functionalities[0]}
+            </p>
+          </div>
         </div>
       </div>
       <button className={moreInfoButton}>Learn more</button>
