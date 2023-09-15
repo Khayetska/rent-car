b296af84cf378dd5727c41ca487464201e165bb4
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-09-15 20:39:07 +0300

    Make modal window with info about chosen car

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sass from './CarItem.module.scss';
 import placeholder from 'images/placeholder.jpeg';
+import ModalWindow from 'components/ModalWindow/ModalWindow';
 
 const {
   carItem,
@@ -17,6 +18,8 @@ const {
 } = sass;
 
 export default function CarItem({ car }) {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   const {
     year,
     make,
@@ -33,42 +36,56 @@ export default function CarItem({ car }) {
   const splittedAddress = address.split(',');
   const [, city, country] = splittedAddress;
 
-  console.log('IntelliSafe advanced safety feature'.slice(0, 27));
+  const handleToggleModal = () => {
+    setIsOpenModal(!isOpenModal);
+  };
+
   return (
-    <li className={carItem}>
-      <div>
-        <div className={carImgWrapper}>
-          <img
-            src={img ?? placeholder}
-            className={carImg}
-            alt={
-              img ? 'View of the car for rent' : 'There is no available image'
-            }
-          />
-          <div className={carImgOverlay}></div>
-        </div>
-        <div className={infoWrapper}>
-          <div className={basicInfo}>
-            <h3 className={carTitle}>
-              {make} {model.length > 12 || make.length > 12 ? <br /> : ''}
-              <span className={carTitleAccent}>{model}</span>, {year}
-            </h3>
-            <p className={carTitle}>{rentalPrice}</p>
+    <>
+      <li className={carItem}>
+        <div>
+          <div className={carImgWrapper}>
+            <img
+              src={img ?? placeholder}
+              className={carImg}
+              alt={
+                img ? 'View of the car for rent' : 'There is no available image'
+              }
+            />
+            <div className={carImgOverlay}></div>
           </div>
-          <div className={extraInfo}>
-            <p className={extraInfoText}>
-              {city} | {country} | {rentalCompany}
-            </p>
-            <p className={extraInfoText}>
-              {type} | {model} | {mileage} |{' '}
-              {functionalities[0].length > 25
-                ? `${functionalities[0].slice(0, 25)}...`
-                : functionalities[0]}
-            </p>
+          <div className={infoWrapper}>
+            <div className={basicInfo}>
+              <h3 className={carTitle}>
+                {make} {model.length > 12 || make.length > 12 ? <br /> : ''}
+                <span className={carTitleAccent}>{model}</span>, {year}
+              </h3>
+              <p className={carTitle}>{rentalPrice}</p>
+            </div>
+            <div className={extraInfo}>
+              <p className={extraInfoText}>
+                {city} | {country} | {rentalCompany}
+              </p>
+              <p className={extraInfoText}>
+                {type} | {model} | {mileage} |{' '}
+                {functionalities[0].length > 25
+                  ? `${functionalities[0].slice(0, 25)}...`
+                  : functionalities[0]}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <button className={moreInfoButton}>Learn more</button>
-    </li>
+        <button className={moreInfoButton} onClick={handleToggleModal}>
+          Learn more
+        </button>
+      </li>
+      {isOpenModal && (
+        <ModalWindow
+          isOpen={isOpenModal}
+          onClose={handleToggleModal}
+          car={car}
+        />
+      )}
+    </>
   );
 }
---
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect } from 'react';
+import sass from './ModalWindow.module.scss';
+import placeholder from 'images/placeholder.jpeg';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const {
+  backdrop,
+  open,
+  modal,
+  closeButton,
+  closeButtonIcon,
+  carImg,
+  carTitle,
+  extraInfoText,
+  carDescription,
+  carSybTitle,
+  conditionsList,
+  conditionsItem,
+  rentalButton,
+} = sass;
+
+export default function ModalWindow({ isOpen = false, onClose, car }) {
+  const {
+    id,
+    year,
+    make,
+    model,
+    type,
+    img,
+    description,
+    accessories,
+    functionalities,
+    rentalPrice,
+    engineSize,
+    fuelConsumption,
+    rentalConditions,
+    address,
+    mileage,
+  } = car;
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => (document.body.style.overflow = 'unset');
+  }, []);
+
+  const splittedConditions = rentalConditions.split('\n');
+  const [text, minAge] = splittedConditions[0].split(': ');
+
+  const splittedAddress = address.split(',');
+  const [, city, country] = splittedAddress;
+
+  const backdropClassName = isOpen ? `${backdrop} ${open}` : backdrop;
+
+  return (
+    <div className={backdropClassName}>
+      <div className={modal}>
+        <button className={closeButton} type="button" onClick={onClose}>
+          <AiOutlineClose size={20} className={closeButtonIcon} />
+        </button>
+        <img
+          src={img ?? placeholder}
+          alt={img ? 'View of the car for rent' : 'There is no available image'}
+          className={carImg}
+        />
+        {/* ---- Title ---- */}
+        <h3 className={carTitle}>
+          {make} <span>{model}</span>, {year}
+        </h3>
+
+        {/* ---- Extra info ----  */}
+        <ul>
+          <li className={extraInfoText}>
+            {city} | {country} | Id: {id} | Year: {year} | Type: {type}
+          </li>
+          <li className={extraInfoText}>
+            Fuel Consumption: {fuelConsumption} | Engine Size: {engineSize}
+          </li>
+        </ul>
+        <p className={carDescription}>{description}</p>
+
+        {/* ---- Accessories and functionalities ---- */}
+        <h4 className={carSybTitle}>Accessories and functionalities:</h4>
+        <ul>
+          <li className={extraInfoText}>
+            {accessories.map((item, index) =>
+              index !== accessories.length - 1 ? `${item} | ` : item
+            )}{' '}
+          </li>
+          <li className={extraInfoText}>
+            {functionalities.map((item, index) =>
+              index !== accessories.length - 1 ? `${item} | ` : item
+            )}
+          </li>
+        </ul>
+
+        {/* ---- Rental Conditions ---- */}
+        <h4 className={carSybTitle}>Rental Conditions:</h4>
+        <ul className={conditionsList}>
+          <li className={conditionsItem}>
+            {text}: <span>{minAge}</span>
+          </li>
+          {splittedConditions.map(
+            (item, index) =>
+              index !== 0 && (
+                <li key={item} className={conditionsItem}>
+                  {item}
+                </li>
+              )
+          )}
+          <li className={conditionsItem}>
+            Mileage: <span>{Intl.NumberFormat('en').format(mileage)}</span>
+          </li>
+          <li className={conditionsItem}>
+            Price: <span>{rentalPrice.split('$')}$</span>
+          </li>
+        </ul>
+        <a href="tel:+380730000000" className={rentalButton}>
+          Rental car
+        </a>
+      </div>
+    </div>
+  );
+}
